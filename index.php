6fb80fd2e1cf608c53faede800fa60d691ad5780
stan67 <stanislas67@outlook.fr>
2021-05-11 15:57:23 +0200

    dev

--- index.php
+++ index.php
@@ -59,6 +59,30 @@
                     </div>
                 </div>
             </div>
+            <!----------------
+                PRESENTATION 
+            ------------------>
+            <div class="uk-section-default">
+                <div class="uk-section uk-light uk-background-cover" style="background-image: url(images/dark.jpg)">
+                    <div class="uk-container">
+
+                        <h3>Section with Images</h3>
+
+                        <div class="uk-grid-match uk-child-width-1-3@m" uk-grid>
+                            <div>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                            </div>
+                            <div>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                            </div>
+                            <div>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
             
             <!----------------
                 HISTORIQUE 
